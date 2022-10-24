638d2f163a1881c24a5818d95cdf727fc49b64bd
Avinash Malla <malla.avi@gmail.com>
2022-10-24 09:10:26 +0300

    Create viomba.py

---
+++ viomba.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_page(startLink, depth, numLinks, tabs):
+    URL = "https://en.wikipedia.org/"
+    page = requests.get(URL + startLink)
+    page.raise_for_status()
+    
+    soup = BeautifulSoup(page.content, "html.parser")
+    all_paragraphs = soup.find("p", attrs={"class":None})
+    links = all_paragraphs.find_all("a", href=re.compile("(^[/wiki/])"), attrs={"class":None})
+    links = [link for link in links if ':' not in link['title']][0:numLinks]
+    
+    pageTitle = soup.title.text.split("-")[0]
+    print("  " * tabs, pageTitle)
+    depth -= 1
+    tabs += 1
+
+    if depth == 0:
+        for link in links:
+            print("  " * tabs, link['title'])
+    else:
+        for link in links:
+            get_page(link['href'], depth, numLinks, tabs)    
+
+
+def main(argv):
+    userInput = str(sys.argv[1])
+    inputLink = 'wiki/'+ userInput
+    depth = int(sys.argv[2])
+    numLinks = int(sys.argv[3])
+    tabs = 0
+    get_page(inputLink, depth, numLinks, tabs)
+
+if __name__ == "__main__":
+    main(sys.argv)
